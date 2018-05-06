c5c18695b6adc8a2c51d5f08340c72c90ac3ca2a
Omar Zeidan <omar@zeidan.io>
2018-05-06 18:18:27 +0200

    Refresh config

--- init.el
+++ init.el
@@ -32,6 +32,16 @@
 ;; Stuff
 ;;
 
+
+;; Don't create backup files in working directory
+(setq backup-directory-alist '(("." . "~/.emacs.d/backup"))
+  backup-by-copying t    ; Don't delink hardlinks
+  version-control t      ; Use version numbers on backups
+  delete-old-versions t  ; Automatically delete excess backups
+  kept-new-versions 20   ; how many of the newest versions to keep
+  kept-old-versions 5    ; and how many of the old
+  )
+
 (setq ring-bell-function 'ignore)
 
 ;; because M-S-< stopped working 
@@ -90,7 +100,7 @@
 
 
 ;;Hippie-Expand
-(global-set-key (kbd "C-S-x") 'hippie-expand)
+;; (global-set-key (kbd "C-S-x") 'hippie-expand)
 
 ;; Backward line kill
 (defun backward-kill-line (arg)
@@ -98,6 +108,9 @@
   (kill-line (- 1 arg)))
 (global-set-key (kbd "C-j") 'backward-kill-line)
 
+(global-set-key (kbd "M-j") 'ace-jump-mode)
+(global-set-key (kbd "M-m") 'iy-go-to-char)
+(global-set-key (kbd "M-q") 'er/expand-region)
 ;; Smart move to line beginning
 ;; C-a now move the point to the beginning of the indentation
 (defun smart-line-beginning ()
@@ -131,7 +144,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(linum ((t (:background "#3f3f3f" :foreground "#666462" :height 100)))))
+ '(linum ((t (:background "#000000" :foreground "#666462" :height 100)))))
 
 
 ;;
@@ -173,11 +186,67 @@
 (setq helm-buffers-fuzzy-matching t
       helm-recentf-fuzzy-match    t)
 
+;; Helm in new frame instead of buffer
+;; 
+;; (setq helm-display-function 'helm-display-buffer-in-own-frame
+;;       helm-display-buffer-reuse-frame t
+;;       helm-use-undecorated-frame-option t)
+
+
+;;
+;; Zoomies
+;; 
+
+(defvar original-font-size nil)
+
+(defun adjust-font-size (delta)
+  (let* ((old-size (face-attribute 'default :height))
+         (new-size (max (max delta (- delta)) (min 200 (+ delta old-size)))))
+    (setq original-font-size (or original-font-size old-size))
+    (set-face-attribute 'default nil :height new-size)
+    (message "Font size set to %d (was %d)" (face-attribute 'default :height) old-size)))
+
+(defun zoom-in ()
+  (interactive)
+  (adjust-font-size +5))
+
+(defun zoom-out ()
+  (interactive)
+  (adjust-font-size -5))
+
+(defun zoom-reset ()
+  (interactive)
+  (when original-font-size
+    (set-face-attribute 'default nil :height original-font-size)))
+
+;; Zoom settings
+(global-set-key [\C-mouse-4] 'zoom-in)
+(global-set-key [\C-mouse-5] 'zoom-out)
+(global-set-key [\C-kp-add] 'zoom-in)
+(global-set-key [\C-kp-subtract] 'zoom-out)
+(global-set-key (kbd "<C-kp-0>") 'zoom-reset)
+(global-set-key (kbd "<C-x +>") 'zoom-in)
+(global-set-key (kbd "<C-x ->") 'zoom-in)
+(global-set-key (kbd "<C-x 0>") 'zoom-reset)
+
+
+
+;; Copy file name to clipboard
+
+;; (defun copy-file-name-to-clipboard ()
+;;   "Copy the current buffer file name to the clipboard."
+;;   (interactive)
+;;   (let ((filename (if (equal major-mode 'dired-mode)
+;;                       default-directory
+;;                     (buffer-file-name))))
+;;     (when filename
+;;       (kill-new filename)
+;;       (message "Copied buffer file name '%s' to the clipboard." filename))))
+
 ;;
 ;; Python
 ;;
 
-(package-initialize)
 (elpy-enable)
 
 (require 'py-autopep8)
@@ -222,30 +291,31 @@
 
 ;; js2-mode
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.cshtml\\'" . web-mode))
 
 
 
 
-(defun my-tern-mode-config ()
-  "For use in 'tern-mode-hook'."
-  (local-set-key (kbd "C-c C-t") 'tern-get-type)
-  (local-unset-key (kbd "C-c C-c"))
-  ;; Todo
-  )
+;; (defun my-tern-mode-config ()
+;;   "For use in 'tern-mode-hook'."
+;;   (local-set-key (kbd "C-c C-t") 'tern-get-type)
+;;   (local-unset-key (kbd "C-c C-c"))
+;;   ;; Todo
+;;   )
 
 
-;; tern
-(add-hook 'js-mode-hook (lambda () (tern-mode t)))
-(add-hook 'tern-mode-hook 'my-tern-mode-config)
+;; ;; tern
+;; (add-hook 'js-mode-hook (lambda () (tern-mode t)))
+;; (add-hook 'tern-mode-hook 'my-tern-mode-config)
 
-(setenv "PATH" (concat (getenv "PATH") ":/home/omar/node_modules/tern/bin/"))
-    (setq exec-path (append exec-path '("/home/omar/node_modules/tern/bin/")))
+;; (setenv "PATH" (concat (getenv "PATH") ":/home/omar/projects/tern/bin"))
+;;     (setq exec-path (append exec-path '("/home/omar/projects/tern/bin")))
 
 
 
 
 ;; company-tern
-(add-to-list 'company-backends 'company-tern)
+;; (add-to-list 'company-backends 'company-tern) 
 
 ;; (require 'indium)
 ;; (add-hook 'js-mode-hook #'indium-interaction-mode)
@@ -272,18 +342,55 @@
 ;; ;; $ cmake . -DCMAKE_EXPORT_COMPILE_COMMANDS=1
 ;; ;; $ rc -J .
 
-;; (add-hook 'c++-mode-hook 'irony-mode)
-;; (add-hook 'c-mode-hook 'irony-mode)
-;; (add-hook 'objc-mode-hook 'irony-mode)
+(add-hook 'c++-mode-hook 'irony-mode)
+(add-hook 'c-mode-hook 'irony-mode)
+(add-hook 'objc-mode-hook 'irony-mode)
 
-;; (defun my-irony-mode-hook ()
-;;   (define-key irony-mode-map [remap completion-at-point]
-;;     'irony-completion-at-point-async)
-;;   (define-key irony-mode-map [remap complete-symbol]
-;;     'irony-completion-at-point-async))
 
-;; (add-hook 'irony-mode-hook 'my-irony-mode-hook)
-;; (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+(add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+
+
+
+(eval-after-load 'company
+  '(add-to-list 'company-backends 'company-irony))
+
+(defun my-irony-mode-hook ()		
+  (define-key irony-mode-map [remap completion-at-point]
+    'irony-completion-at-point-async)
+  (define-key irony-mode-map [remap complete-symbol]
+    'irony-completion-at-point-async)
+  (define-key irony-mode-map (kbd "M-.") 'rtags-find-symbol-at-point)
+  (define-key irony-mode-map (kbd "M-,") 'rtags-location-stack-back)
+  (define-key irony-mode-map (kbd "C-c C-r" 'rtags-find-references-at-point)))
+
+(add-hook 'irony-mode-hook 'my-irony-mode-hook)
+(add-hook 'irony-mode-hook 'rtags-start-process-unless-running)
+
+
+
+;; C#
+
+(add-hook 'csharp-mode-hook 'omnisharp-mode)
+(add-hook 'omnisharp-mode-hook 'flycheck-mode)
+(eval-after-load
+ 'company
+ '(add-to-list 'company-backends 'company-omnisharp))
+
+(defun my-csharp-mode-setup ()
+  (setq indent-tabs-mode nil)
+  (setq c-syntactic-indentation t)
+  (c-set-style "ellemtel")
+  (setq c-basic-offset 4)
+  (setq truncate-lines t)
+  (setq tab-width 4)
+  (setq evil-shift-width 4)
+  (local-set-key (kbd "C-c C-c") 'recompile)
+  (define-key omnisharp-mode-map (kbd "M-.") 'omnisharp-go-to-definition)
+  (define-key omnisharp-mode-map (kbd "C-M-.") 'omnisharp-go-to-definition-other-window))
+
+(add-hook 'csharp-mode-hook 'my-csharp-mode-setup t)
+
+
 
 ;; ;; Irony also needs:
 ;; ;; $ cd /path/to/project/root
@@ -337,15 +444,29 @@
 ;; cargo install racer
 ;; rustup component add rust-src
 
-(add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
-(add-hook 'rust-mode-hook  #'racer-mode)
-(add-hook 'rust-mode-hook  #'flycheck-mode)
-(add-hook 'racer-mode-hook #'eldoc-mode)
-(add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
-(add-hook 'rust-mode-hook
-          '(lambda ()
-             (local-set-key (kbd "TAB") #'company-indent-or-complete-common)
-             (cargo-minor-mode 1)))
+;; Go
+(defun set-exec-path-from-shell-PATH ()
+  (let ((path-from-shell (replace-regexp-in-string
+                          "[ \t\n]*$"
+                          ""
+                          (shell-command-to-string "$SHELL --login -i -c 'echo $PATH'"))))
+    (setenv "PATH" path-from-shell)
+    (setq eshell-path-env path-from-shell) ; for eshell users
+    (setq exec-path (split-string path-from-shell path-separator))))
+
+(when window-system (set-exec-path-from-shell-PATH))
+(setenv "GOPATH" "/home/omar/projects/go/")
+(add-to-list 'exec-path "/home/omar/projects/go/bin/")
+
+(require 'company-go)
+
+(add-hook 'before-save-hook 'gofmt-before-save)
+(add-hook 'go-mode-hook (lambda ()
+                          (local-set-key (kbd "M-.") 'godef-jump)))
+(add-hook 'go-mode-hook 'flycheck-mode)
+(add-hook 'go-mode-hook (lambda ()
+                          (set (make-local-variable 'company-backends) '(company-go))
+                          (company-mode)))
 
 
 (custom-set-variables
@@ -353,7 +474,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(company-echo-delay 0 t)
  '(company-idle-delay 0)
+ '(company-minimum-prefix-length 1)
  '(custom-safe-themes
    (quote
     ("203dfe036c9a450718efdd871a89ee7a6599129ac605c041d543d09831070a44" default)))
